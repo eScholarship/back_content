de49088ed3819faaa29b2506cc35d871dc0aae13
Andy Byers <andy.byers@openlibhums.org>
2017-06-22 13:00:40 +0100

    Main author is now a required field.

--- plugin_settings.py
+++ plugin_settings.py
@@ -4,6 +4,9 @@ AUTHOR = 'Andy Byers'
 VERSION = '1.0'
 SHORT_NAME = 'back_content'
 MANAGER_URL = 'bc_index'
+IS_WORKFLOW_PLUGIN = True
+HANDSHAKE_URL = 'bc_article'
+STAGE = 'Back Content'
 
 from utils import models
 
