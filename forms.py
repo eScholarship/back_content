32f20517319b75006b4318eb3fd084144d29f5eb
Andy Byers <a.byers@bbk.ac.uk>
2024-09-05 16:27:12 +0100

    Updates plugin for 1.7 release.

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from submission import models
 from review.logic import render_choices
